97e3067c15e357ee1a3a5f50ff31f0d61029daf6
galina215 <49083430+galina215@users.noreply.github.com>
2020-06-30 13:37:20 +0800

    更新laravel20200630

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,13 @@
+@include('frontend.layouts.master')
+
+@section('title','about')
+
+@section('content')
+    <section class="page-section clearfix">
+      hello! hello! hello! hello! hello! hello! hello! hello! hello!
+    </section>
+    <section class="page-section cta">
+
+    </section>
+
+@endsection
---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,13 @@
+@include('frontend.layouts.master')
+
+@section('title','Home')
+
+@section('content')
+    <section class="page-section clearfix">
+      hello!
+    </section>
+    <section class="page-section cta">
+
+    </section>
+
+@endsection
